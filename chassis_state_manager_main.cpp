d8e09db1904927c32352f0971349ee1d13b5b144
HCLOpenBMC <HCL_OpenBMC@hcl.com>
2020-05-12 10:44:50 +0530

    dharshan - code changes

--- chassis_state_manager_main.cpp
+++ chassis_state_manager_main.cpp
@@ -5,19 +5,21 @@
 #include "config.h"
 #include "chassis_state_manager.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
     auto bus = sdbusplus::bus::new_default();
+	std::string node = argv[1];
 
     // For now, we only have one instance of the chassis
-    auto objPathInst = std::string{CHASSIS_OBJPATH} + '0';
+    auto objPathInst = std::string{CHASSIS_OBJPATH} + node;
 
     // Add sdbusplus ObjectManager.
     sdbusplus::server::manager::manager objManager(bus, objPathInst.c_str());
 
     phosphor::state::manager::Chassis manager(bus, objPathInst.c_str());
 
-    bus.request_name(CHASSIS_BUSNAME);
+	auto busName = std::string{CHASSIS_BUSNAME} + node;
+    bus.request_name(busName.c_str());
     manager.startPOHCounter();
 
     return 0;
--- host_state_manager_main.cpp
+++ host_state_manager_main.cpp
@@ -6,14 +6,14 @@
 #include "config.h"
 #include "host_state_manager.hpp"
 
-int main()
+int main(int argc, char *argv[])
 {
     namespace fs = std::experimental::filesystem;
-
+	std::string node = argv[1];
     auto bus = sdbusplus::bus::new_default();
 
     // For now, we only have one instance of the host
-    auto objPathInst = std::string{HOST_OBJPATH} + '0';
+    auto objPathInst = std::string{HOST_OBJPATH} + node;
 
     // Add sdbusplus ObjectManager.
     sdbusplus::server::manager::manager objManager(bus, objPathInst.c_str());
@@ -23,7 +23,8 @@ int main()
     auto dir = fs::path(HOST_STATE_PERSIST_PATH).parent_path();
     fs::create_directories(dir);
 
-    bus.request_name(HOST_BUSNAME);
+	auto busName = std::string{HOST_BUSNAME} + node;
+    bus.request_name(busName.c_str());
 
     while (true)
     {
